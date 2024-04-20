6e9a5586e3fe1bf111549f48fa56fd5c4e804dcd
abeba damasse <abeba.dms@icloud.com>
2024-04-20 15:03:05 +0200

    IV. CREATION D’UN COMPOSANT

--- app/jxad_angular_avril/src/app/app.routes.ts
+++ app/jxad_angular_avril/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './pages/general/accueil/accueil.component';
 import { NotFoundComponent } from './pages/not_found/not-found/not-found.component';
+import { PremierComposantComponent } from './pages/specific/premier-composant/premier-composant.component';
 
 export const routes: Routes = [
     { path: '', component: AccueilComponent},
-    { path: 'NotFound', component: NotFoundComponent},
+
+    {path: 'premier-composant', component: PremierComposantComponent}, 
+    
+    { path: '***', component: NotFoundComponent},
 ];
--- app/jxad_angular_avril/src/app/pages/specific/premier-composant/premier-composant.component.ts
+++ app/jxad_angular_avril/src/app/pages/specific/premier-composant/premier-composant.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MonPremierComposantComponent } from '../../../components/mon-premier-composant/mon-premier-composant.component';
 
 @Component({
   selector: 'app-premier-composant',
   standalone: true,
-  imports: [],
+  imports: [MonPremierComposantComponent],
   templateUrl: './premier-composant.component.html',
   styleUrl: './premier-composant.component.css'
 })
